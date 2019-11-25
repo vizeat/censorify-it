f79dc6511a0b1cbfbdec176564657714738e2bed
Jérémie Parker <github@jeremie-parker.com>
2019-11-25 12:21:19 +0100

    fix(phone): update the phone regex

--- src/index.js
+++ src/index.js
@@ -1,64 +1,59 @@
-import LinkifyIt from 'linkify-it';
-const REPLACEMENT_TEXT = '⏤⏤⏤⏤';
-const telRegex = /(\(?\+?[0-9]+\)?)+[0-9_\- ()]{6,}[0-9]/g;
-const dateRegex = /(2\d{3})-(0[1-9]|1[0-2])-(0[1-9]|[1-2]\d|3[0-1])/
+import LinkifyIt from 'linkify-it'
+const REPLACEMENT_TEXT = '⏤⏤⏤⏤'
+const telRegex = /(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4,5}/g
 
-function CensorifyIt() {
-  LinkifyIt.call(this);
+function CensorifyIt () {
+  LinkifyIt.call(this)
   this.add('+', {
     validate: function (text, pos, self) {
       if (!self.re.phone) {
-        self.re.phone = new RegExp(telRegex);
+        self.re.phone = new RegExp(telRegex)
       }
 
-      if (self.re.phone.test(text) && !dateRegex.test(text)) {
-        return text.match(self.re.phone)[0].length;
+      if (self.re.phone.test(text)) {
+        return text.match(self.re.phone)[0].length
       }
 
-      return 0;
-    }
-  });
-  this.add('(', '+');
+      return 0
+    },
+  })
+  this.add('(', '+')
 
   for (let i = 0; i < 10; i++) {
-    this.add(i.toString(), '+');
+    this.add(i.toString(), '+')
   }
 
-  this.replacementText = REPLACEMENT_TEXT;
+  this.replacementText = REPLACEMENT_TEXT
 }
 
-CensorifyIt.prototype = Object.create(LinkifyIt.prototype);
+CensorifyIt.prototype = Object.create(LinkifyIt.prototype)
 
-CensorifyIt.prototype.set = function set({
-  exception,
-  replacementText = REPLACEMENT_TEXT,
-  ...options
-}) {
-  LinkifyIt.prototype.set.call(this, options);
-  this.exception = exception;
-  this.replacementText = replacementText;
-  return this;
-};
-
-function Match(match, text) {
-  this.schema = match.schema;
-  this.index = match.index;
-  this.lastIndex = match.lastIndex;
-  this.raw = match.text;
-  this.text = text.trim();
+CensorifyIt.prototype.set = function set ({ exception, replacementText = REPLACEMENT_TEXT, ...options }) {
+  LinkifyIt.prototype.set.call(this, options)
+  this.exception = exception
+  this.replacementText = replacementText
+  return this
 }
 
-CensorifyIt.prototype.match = function match(text) {
-  if (!text) return [];
-  const matches = LinkifyIt.prototype.match.call(this, text);
-  if (!matches) return [];
+function Match (match, text) {
+  this.schema = match.schema
+  this.index = match.index
+  this.lastIndex = match.lastIndex
+  this.raw = match.text
+  this.text = text.trim()
+}
+
+CensorifyIt.prototype.match = function match (text) {
+  if (!text) return []
+  const matches = LinkifyIt.prototype.match.call(this, text)
+  if (!matches) return []
   return matches.map((match, i) => {
     if (this.exception && match.text.match(this.exception)) {
-      return match;
+      return match
     }
 
-    return new Match(match, this.replacementText);
-  });
-};
+    return new Match(match, this.replacementText)
+  })
+}
 
-export default CensorifyIt;
+export default CensorifyIt
